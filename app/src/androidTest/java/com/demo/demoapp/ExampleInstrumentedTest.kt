9f6151453493240a13c3730dc9b7515fb47bba3f
Chetan Gaikwad <CGaikwad@equalexperts.com>
2019-08-19 23:57:58 +0530

    Added Espresso test

--- app/src/androidTest/java/com/demo/demoapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/demo/demoapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.demo.demoapp
 
 import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,36 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(
+        MainActivity::class.java,
+        true, // initialTouchMode
+        true
+    )
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.demo.demoapp", appContext.packageName)
+    }
+
+    @Test
+    fun clickingSearch_shouldReturnValidResult() {
+
+        Espresso.onView(ViewMatchers.withId(R.id.imgSearch))
+            .perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.edtSearch))
+            .perform(ViewActions.typeText("Cat"))
+
+        Espresso.onView(ViewMatchers.withId(R.id.btnSearch))
+            .perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.txtTag))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Cat")))
+
+
     }
 }
